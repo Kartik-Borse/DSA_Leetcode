09c9dae33c837191927a97cc7d253a5fe26a1eb0
Kartik Borse <kartikborse056@gmail.com>
2026-02-13 22:23:14 +0530

    Time: 1 ms (99.79%) | Memory: 42.6 MB (62.60%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -5,8 +5,10 @@ public class Solution {
         while (x != 0) {
             int digit = x % 10;
 
+           
             int newResult = result * 10 + digit;
 
+            
             if ((newResult - digit) / 10 != result) {
                 return 0;
             }
